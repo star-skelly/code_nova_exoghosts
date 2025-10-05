7d2219f9555fe1d83281887301bf7b366b16aaac
xiaziyna <xiaziyna@googlemail.com>
2025-10-05 10:30:38 -0400

    TESS data

--- noise_sim.py
+++ noise_sim.py
@@ -1,9 +1,8 @@
 import numpy as np
 from noise_generator import powerlaw_psd, simulate_from_psd
-
-# fixed params (previously CLI args)
+import matplotlib.pyplot as plt
 NUM = 1000
-N = 90*24*30
+N = 90*24*2
 FS = 1/120
 ALPHA = 2.0
 VAR = 1.0
@@ -16,6 +15,7 @@ data = np.vstack([
     simulate_from_psd(S, N, FS, rng=np.random.default_rng(rng.integers(0, 2**63 - 1)))
     for _ in range(NUM)
 ])
+
 np.savez_compressed(OUT, data=data, n_samples=N, fs=FS, alpha=ALPHA, variance=VAR, seed=SEED,
                     description="Spectral noise cohort generated via powerlaw PSD and simulate_from_psd")
 
